f2462d6f139df387861c4bdbc2b019d9e5d2276d
25288 <2528851339@qq.com>
2021-01-26 18:45:57 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * HelloGit
+ *
+ * @author 25288
+ * @date 2021/1/26 18:38
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
